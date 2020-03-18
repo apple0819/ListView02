05308d4e07be08a252a71299366f654ba1b33931
apple0819 <apple0819@users.noreply.github.com>
2020-03-18 15:46:58 +0900

    방 데이터 추가 => roomDatas에 대입

--- app/src/main/java/com/example/listview02/MainActivity.java
+++ app/src/main/java/com/example/listview02/MainActivity.java
@@ -38,5 +38,19 @@ public class MainActivity extends BaseActivity {
         roomAdapter = new RoomAdapter(mContext, R.layout.room_list_item, roomDatas);
         binding.roomListView.setAdapter(roomAdapter);
 
+        addRooms();
     }
+
+    private void addRooms() {
+        roomDatas.add(new Room(8000, "서울시 은평구", 1, "살기 좋은 방입니다."));
+        roomDatas.add(new Room(18000, "서울시 도봉구", 2, "살기 좋은 방입니다."));
+        roomDatas.add(new Room(4000, "경기도 부천시", -1, "싸게나온 방입니다."));
+        roomDatas.add(new Room(45000, "경기도 고양시", 0, "살기 좋은 방입니다."));
+        roomDatas.add(new Room(800000, "서울시 강남구", 4, "비싼 방입니다."));
+
+        roomAdapter.notifyDataSetChanged();
+
+    }
+
+
 }
