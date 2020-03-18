f114947ccf1ad6e93d9b03d0471d798cfece335f
apple0819 <apple0819@users.noreply.github.com>
2020-03-18 14:44:21 +0900

    Initial commit

---
+++ app/src/main/java/com/example/listview02/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.listview02;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+
+import com.example.listview02.databinding.ActivityMainBinding;
+
+public class MainActivity extends BaseActivity {
+
+    ActivityMainBinding binding = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        setupEvents();
+        setValues();
+
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+}
